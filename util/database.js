d391b97bf8eeedd0bee7fd8746d517ec7f47e427
misxka <mishutkavlesu@gmail.com>
2021-04-09 23:55:29 +0300

    feat: connect to database

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'airport-schedule',
+  password: '11111111'
+});
+
+module.exports = pool.promise();
